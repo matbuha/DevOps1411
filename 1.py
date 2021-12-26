3c789ed1cd5a82e98560993ca2937c30696803b1
ariel <95013498+matbuha@users.noreply.github.com>
2021-12-26 18:35:38 +0200

    fghlgflhk

---
+++ 1.py
@@ -0,0 +1,25 @@
+print("hello")
+david = 10
+print(david)
+my_name = "ariel"
+print(my_name)
+is_true = False
+my_list = ["ariel", "eli", 31, True]
+print(my_list[1])
+my_dict = {"name": "ariel",
+           "lname": "ben zikri",
+           "age": 34,
+           "is_married": True}
+print(my_dict["name"])
+print(my_dict.keys())
+my_number = 5 * 2
+my_other = 5 * "ariel"
+print(my_other)
+
+if my_number == 10:
+    print("my_number")
+
+fname = "ariel"
+lname = "ben zikri"
+print(f"hello {fname} {lname}")
+print("hello %s %s" % (fname, lname))
