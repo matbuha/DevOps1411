ca29a211b2a892be704d8a0c97296875b74b8c80
ariel <95013498+matbuha@users.noreply.github.com>
2021-12-26 18:48:29 +0200

    Create 16.py

---
+++ 16.py
@@ -0,0 +1,25 @@
+input_from_user = int(input("enter  your age: "))
+
+if input_from_user > 0:
+    print("age is ok")
+
+input_from_user = int(input("enter  your amount of pets: "))
+
+if input_from_user > 0:
+    print("amount of pets is ok")
+
+
+def check_input(gt, input_string):
+    input_from_user = int(input(input_string))
+    if input_from_user > gt:
+        return True
+    else:
+        return False
+
+
+result = check_input(0, "enter your age: ")
+if result == True:
+    print("age is ok")
+result = check_input(2, "enter  your amount of pets: ")
+if result == True:
+    print("amount of pets is ok")
