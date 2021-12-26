b7cb24f6590ebf0fefe0836c64400545e99f546a
ariel <95013498+matbuha@users.noreply.github.com>
2021-12-26 20:44:27 +0200

    Update class_assigment.py

--- class_assigment.py
+++ class_assigment.py
@@ -1,3 +1,11 @@
 def my_func():
     print("hello world")
-    
+    properties([[$class: 'JobLocalConfiguration', changeReasonComment: ''], pipelineTriggers([pollSCM('* * * * * ')])])
+node {
+    stage("clone"){
+        checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/matbuha/DevOps1411.git']]])
+    }
+    stage("show files"){
+        bat "dir"
+    }
+}
